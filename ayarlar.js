926085fba18fd3ef9c94a18df80868922f910d9e
Lizzard <101275325+lizzard-development@users.noreply.github.com>
2025-05-03 21:15:35 +0300

    Create ayarlar.js

---
+++ ayarlar.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dosyaEngel: {
+    durum: false,
+    kanallar: []
+  },
+  kufurEngel: {
+    durum: false,
+    kanallar: []
+  },
+  etiketEngel: {
+    kullanicilar: {}
+  },
+  everyoneEngel: {
+    durum: false
+  },
+  reklamEngel: {
+    kanallar: {}
+  },
+  selfRoller: []
+};
